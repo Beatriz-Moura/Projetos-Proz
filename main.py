e2ffd030cd3164fdf113b5451566a77d32028be1
Beatriz-Moura <147749821+Beatriz-Moura@users.noreply.github.com>
2023-10-12 14:53:20 -0400

    Projeto de repetição de laços
    Foi pedido 3 maneiras mas como a função do while não existe na python não fiz

---
+++ main.py
@@ -0,0 +1,13 @@
+andar = 21
+for i in range(20,0,-1):
+    andar = andar - 1
+    if (andar == 13):
+        continue
+    print (andar,"° andar ")
+    
+andar = 21  
+while (andar > 1):
+    andar = andar - 1
+    if (andar == 13):
+      continue  
+    print (andar, "° andar ")
